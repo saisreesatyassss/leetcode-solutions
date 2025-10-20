3c67153030854ed225228869fd560f9c8ab166f6
saisreesatya <132558766+saisreesatyassss@users.noreply.github.com>
2025-10-20 16:06:59 +0530

    File updated at 10/20/2025, 4:06:59 PM

--- 1-TwoSum/1-TwoSum.java
+++ 1-TwoSum/1-TwoSum.java
@@ -1,13 +1,17 @@
-// Last updated: 10/20/2025, 4:05:19 PM
+// Last updated: 10/20/2025, 4:06:59 PM
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        for (int i = 0; i < nums.length; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                if (nums[i] + nums[j] == target) {
-                    return new int[]{i, j};
+        
+        for(int i=0;i<nums.length;i++){
+             
+            for(int j=i+1;j<nums.length;j++){
+                if(nums[i]+nums[j]==target){
+                    return new int[]{j, i};
                 }
             }
         }
-        throw new IllegalArgumentException("No two sum solution");
+
+    return new int[]{0, 0};
+
     }
 }
