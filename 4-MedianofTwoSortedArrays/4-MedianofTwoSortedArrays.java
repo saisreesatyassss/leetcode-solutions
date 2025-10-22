c736b14b45d057cb4da04d3a3eef839eb8696695
saisreesatya <132558766+saisreesatyassss@users.noreply.github.com>
2025-10-22 14:57:07 +0530

    File updated at 10/22/2025, 2:57:07 PM

--- 4-MedianofTwoSortedArrays/4-MedianofTwoSortedArrays.java
+++ 4-MedianofTwoSortedArrays/4-MedianofTwoSortedArrays.java
@@ -1,22 +1,20 @@
-// Last updated: 10/22/2025, 2:32:53 PM
-import java.util.Arrays;
-
+// Last updated: 10/22/2025, 2:57:07 PM
 class Solution {
-    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int[] arr = new int[nums1.length + nums2.length];
+    public int reverse(int x) {
+        String str = Integer.toString(x);
+        StringBuilder res = new StringBuilder();
 
-        // Copy elements
-        for (int i = 0; i < nums1.length; i++) arr[i] = nums1[i];
-        for (int i = 0; i < nums2.length; i++) arr[nums1.length + i] = nums2[i];
+        for (int i = str.length() - 1; i >= 0; i--) {
+            res.append(str.charAt(i));
+        }
 
-        // Sort the combined array
-        Arrays.sort(arr);
-
-        int n = arr.length;
-        if (n % 2 == 0) {
-            return (arr[n/2 - 1] + arr[n/2]) / 2.0;
-        } else {
-            return arr[n/2];
+        String cleaned = res.toString().replace("-", "");
+        try {
+            int number = Integer.parseInt(cleaned);
+            return x < 0 ? -number : number;
+        } catch (NumberFormatException e) {
+            // Overflow case
+            return 0;
         }
     }
 }
