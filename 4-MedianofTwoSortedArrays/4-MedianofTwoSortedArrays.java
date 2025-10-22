4850edd2e6489ac8ca6b7213682559c29e135a39
saisreesatya <132558766+saisreesatyassss@users.noreply.github.com>
2025-10-22 14:58:31 +0530

    File updated at 10/22/2025, 2:58:31 PM

--- 4-MedianofTwoSortedArrays/4-MedianofTwoSortedArrays.java
+++ 4-MedianofTwoSortedArrays/4-MedianofTwoSortedArrays.java
@@ -1,19 +1,18 @@
-// Last updated: 10/22/2025, 2:57:07 PM
+// Last updated: 10/22/2025, 2:58:31 PM
 class Solution {
     public int reverse(int x) {
         String str = Integer.toString(x);
         StringBuilder res = new StringBuilder();
-
         for (int i = str.length() - 1; i >= 0; i--) {
             res.append(str.charAt(i));
         }
 
         String cleaned = res.toString().replace("-", "");
+
         try {
             int number = Integer.parseInt(cleaned);
             return x < 0 ? -number : number;
         } catch (NumberFormatException e) {
-            // Overflow case
             return 0;
         }
     }
