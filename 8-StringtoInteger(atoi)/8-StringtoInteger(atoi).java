f97a0a24b6ffbd0f22174b6cdc222a803ceda800
saisreesatya <132558766+saisreesatyassss@users.noreply.github.com>
2025-10-23 17:24:44 +0530

    File updated at 10/23/2025, 5:24:43 PM

--- 8-StringtoInteger(atoi)/8-StringtoInteger(atoi).java
+++ 8-StringtoInteger(atoi)/8-StringtoInteger(atoi).java
@@ -1,21 +1,14 @@
-// Last updated: 10/23/2025, 5:20:16 PM
+// Last updated: 10/23/2025, 5:24:43 PM
 class Solution {
-    public int myAtoi(String s) {
-     int sign=1,i = 0;;
-     s=s.trim();
-     long res=0;
-
-     if (s.length() == 0) return 0;
-     if (s.charAt(0) == '-') { sign = -1; i++; } 
-        else if (s.charAt(0) == '+') { i++; }
-
-    while(i<s.length()){
-        char ch = s.charAt(i); 
-        if (ch < '0' || ch > '9') break;
-        res = res * 10 + (ch - '0');
-        if (sign * res > Integer.MAX_VALUE) return Integer.MAX_VALUE; // Handle overflow 
-        if (sign * res < Integer.MIN_VALUE) return Integer.MIN_VALUE; i++; 
+    public boolean isPalindrome(int x) {
         
-    }return (int) (sign * res);
+        String str = String.valueOf(x);
+        String res="";
+
+        for(int i=str.length()-1;i>=0;i--){
+            res=res+str.charAt(i);
+        }
+
+    return res.equals(str);
     }
 }
