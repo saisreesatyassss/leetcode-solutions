a3127eb611da41390b76d574bb9fbb3edd3c880b
saisreesatya <132558766+saisreesatyassss@users.noreply.github.com>
2025-10-28 18:10:08 +0530

    File updated at 10/28/2025, 6:10:08 PM

--- 1-TwoSum/1-TwoSum.java
+++ 1-TwoSum/1-TwoSum.java
@@ -1,22 +1,16 @@
-// Last updated: 10/20/2025, 5:12:48 PM
-import java.util.Arrays;
-
+// Last updated: 10/28/2025, 6:10:08 PM
 class Solution {
-    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int[] arr = new int[nums1.length + nums2.length];
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer,Integer> map= new HashMap<>();
+        for( int i=0;i<nums.length;i++){
 
-        // Copy elements
-        for (int i = 0; i < nums1.length; i++) arr[i] = nums1[i];
-        for (int i = 0; i < nums2.length; i++) arr[nums1.length + i] = nums2[i];
+            if(map.containsKey(target-nums[i])){
+                return new int[]{map.get(target-nums[i]),i};
 
-        // Sort the combined array
-        Arrays.sort(arr);
+            }
 
-        int n = arr.length;
-        if (n % 2 == 0) {
-            return (arr[n/2 - 1] + arr[n/2]) / 2.0;
-        } else {
-            return arr[n/2];
-        }
+            map.put(nums[i],i);  
+        } 
+        return new int[]{};
     }
 }
